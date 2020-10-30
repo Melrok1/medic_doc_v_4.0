2776d5b9758bfd11ebc222512ee9c34640265726
Martin Blascak <martin.blascak86@gmail.com>
2020-10-30 23:10:34 +0100

    add firebase init

---
+++ src/firebase/init.js
@@ -0,0 +1,23 @@
+import firebase from '@firebase/app';
+import 'firebase/auth';
+import 'firebase/storage'; 
+import 'firebase/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3ossYi3gZPtq6g8szcYiWexi6geKM-gI",
+  authDomain: "emilia-blascakova-clinic.firebaseapp.com",
+  databaseURL: "https://emilia-blascakova-clinic.firebaseio.com",
+  projectId: "emilia-blascakova-clinic",
+  storageBucket: "emilia-blascakova-clinic.appspot.com",
+  messagingSenderId: "737491160785",
+  appId: "1:737491160785:web:82e67db12a74ca642d3ff0",
+  measurementId: "G-4ZEVJ4HRQ5"
+};
+
+// Initialize Firebase
+const fb = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth()
+
+export {fb, db, auth};
